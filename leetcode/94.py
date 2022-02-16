450709686341afaf971ab3c35912fb32d737ed6f
daweedkob <71332326+daweedkob@users.noreply.github.com>
2022-02-16 16:42:48 +0400

    Added leetcode/94.py

---
+++ leetcode/94.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> list[int]:
+        res = []
+        nodes = []
+        curr = root
+        if not root:
+            return res
+
+        while nodes or curr:
+            while curr:
+                nodes.append(curr)
+                curr = curr.left
+
+            curr = nodes.pop()
+            res.append(curr.val)
+            curr = curr.right
+
+        return res
