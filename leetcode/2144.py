8f5d3f3e0d20e067b84eab772bb35fca3c5fd5b9
daweedkob <71332326+daweedkob@users.noreply.github.com>
2022-03-28 16:42:48 +0400

    Added leetcode/2144.py

---
+++ leetcode/2144.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimumCost(self, cost: list[int]) -> int:
+        heapq._heapify_max(cost)
+        res = 0
+        for i in range(1, len(cost) + 1):
+            if i % 3:
+                res += heapq._heappop_max(cost)
+            else:
+                heapq._heappop_max(cost)
+
+        return res
