d6a5ff37a3effb11866d8f60638d3a16d77907bf
daweedkob <71332326+daweedkob@users.noreply.github.com>
2022-03-29 16:42:48 +0400

    Added leetcode/27.py

---
+++ leetcode/27.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        k = 0
+        for i in nums.copy():
+            if i == val:
+                nums.remove(i)
+            else:
+                k += 1
+        return k
