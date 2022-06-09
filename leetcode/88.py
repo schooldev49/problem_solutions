eb7cf2997bc36c97c34d78ce485c0f5bf83b6866
daweedkob <71332326+daweedkob@users.noreply.github.com>
2022-06-09 16:42:48 +0400

    Added leetcode/88.py

---
+++ leetcode/88.py
@@ -0,0 +1,9 @@
+class Solution:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        for i in range(n):
+            nums1.pop()
+        nums1.extend(nums2)
+        nums1.sort()
