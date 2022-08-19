4fb13e79c19ebca7662ab4eb09ac54b54e90d264
daweedkob <71332326+daweedkob@users.noreply.github.com>
2022-08-19 16:42:48 +0400

    Added leetcode/2351.py

---
+++ leetcode/2351.py
@@ -0,0 +1,8 @@
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        a = ""
+        for i in s:
+            if i in a:
+                return i
+            a += i
+        return i
