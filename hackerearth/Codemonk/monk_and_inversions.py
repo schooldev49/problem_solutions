858403c1041e97d60d0acd5818049bd7fe55f72f
meggyyyyy <85836852+meggyyyyy@users.noreply.github.com>
2022-10-07 21:01:12 +0400

    Resolved conversation

--- hackerearth/Codemonk/monk_and_inversions.py
+++ hackerearth/Codemonk/monk_and_inversions.py
@@ -1,16 +1,12 @@
 for i in range(int(input())):
     n = int(input())
-    m = []
-    for i in range(n):
-        m.append(list(map(int, input().split())))
-    
+    m = [list(map(int, input().split())) for i in range(n)]
     count = 0
-
     for i in range(n):
         for j in range(n):
-            for a in range(i,n):
-                for b in range(j,n):
+            for a in range(i, n):
+                for b in range(j, n):
                     if m[i][j] > m[a][b]:
-                        count +=1
+                        count += 1
             
     print(count)
